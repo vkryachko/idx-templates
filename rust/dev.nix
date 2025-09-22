88462c72ec19706eb382ec72ac8b2f171478a0ce
Vova Kryachko <v.kryachko@gmail.com>
2025-09-22 15:35:47 -0400

    fix

--- rust/dev.nix
+++ rust/dev.nix
@@ -1,6 +1,6 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 let
   ttyd = pkgs.writeShellScript "code-oss" ''
     rm -rf /run/idx/code-oss.sock 
