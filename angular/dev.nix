23d03729742cd8c18093a7d31f9348daff94fb45
Vova Kryachko <v.kryachko@gmail.com>
2025-09-22 15:20:08 -0400

    Test

--- angular/dev.nix
+++ angular/dev.nix
@@ -1,6 +1,30 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
-{ pkgs, ... }: {
+{ pkgs, lib, ... }:
+let
+  ttyd = pkgs.writeShellScript "code-oss" ''
+    rm -rf /run/idx/code-oss.sock 
+    ${
+      /*
+        This files needs to be readable and writable by nginx to be able to proxy to the unix
+         domain socket. As soon as the file is created and chmod'ed, this bash subshell wil exit.
+      */
+      ""
+    } 
+    { 
+      while [ ! -S /run/idx/code-oss.sock ]; 
+        do sleep 1; 
+      done 
+      chmod a+rw /run/idx/code-oss.sock 
+    } & 
+
+    cmd=( 
+      ${pkgs.ttyd}/bin/ttyd -t fontFamily="FiraCode Nerd Font" -i /run/idx/code-oss.sock -W gemini 
+    ) 
+    exec "''${cmd[@]}" 
+  '';
+in
+{
   # Which nixpkgs channel to use.
   channel = "stable-24.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
@@ -8,30 +32,11 @@
     pkgs.nodejs_20
   ];
   # Sets environment variables in the workspace
-  env = {};
-  idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [
-      "angular.ng-template"
-    ];
-    workspace = {
-      # Runs when a workspace is first created with this `dev.nix` file
-      onCreate = {
-        npm-install = "npm ci --no-audit --prefer-offline --no-progress --timing || npm i --no-audit --no-progress --timing";
-        # Open editors for the following files by default, if they exist:
-        default.openFiles = [ "src/app/app.component.ts" ];
-      };
-      # To run something each time the workspace is (re)started, use the `onStart` hook
-    };
-    # Enable previews and customize configuration
-    previews = {
-      enable = true;
-      previews = {
-        web = {
-          command = ["npm" "run" "start" "--" "--port" "$PORT" "--host" "0.0.0.0"];
-          manager = "web";
-        };
-      };
+  env = { };
+  processes.code-oss = lib.mkForce {
+    command = "${ttyd}";
+    env = {
+      COLORTERM = "truecolor";
     };
   };
 }
