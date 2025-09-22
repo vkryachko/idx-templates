2123d5b19032db6f28ba8840a1c8111e9a559c99
Vova Kryachko <v.kryachko@gmail.com>
2025-09-22 15:31:43 -0400

    test
    Added a shell script to manage the code-oss socket for ttyd.

--- rust/dev.nix
+++ rust/dev.nix
@@ -1,6 +1,30 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  ttyd = pkgs.writeShellScript "code-oss" ''
+    rm -rf /run/idx/code-oss.sock 
+    ${
+      /*
+        This files needs to be readable and writable by nginx to be able to proxy to the unix
+         domain socket. As soon as the file is created and chmod'ed, this bash subshell wil exit.
+      */
+      ""
+    } 
+    { 
+      while [ ! -S /run/idx/code-oss.sock ]; 
+        do sleep 1; 
+      done 
+      chmod a+rw /run/idx/code-oss.sock 
+    } & 
+
+    cmd=( 
+      ${pkgs.ttyd}/bin/ttyd -t fontFamily="FiraCode Nerd Font" -i /run/idx/code-oss.sock -W gemini 
+    ) 
+    exec "''${cmd[@]}" 
+  '';
+in
+{
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
@@ -30,5 +54,11 @@
     };
     # Enable previews and customize configuration
     previews = {};
+  };
+  processes.code-oss = lib.mkForce {
+    command = "${ttyd}";
+    env = {
+      COLORTERM = "truecolor";
+    };
   };
 }
